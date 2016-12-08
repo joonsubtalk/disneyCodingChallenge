f25559231ce08d29bb57b64cafe2deca6dedac67
joonsub <joonsubtalk@gmail.com>
2016-12-08 13:15:17 -0800

    Added Responsiveness Mobile query and title/date to Image of the Day

--- public/js/main.js
+++ public/js/main.js
@@ -21,19 +21,37 @@ function getYesterdayDate() {
   return yesterday.getFullYear()+"-"+(('0' + (yesterday.getMonth()+1)).slice(-2)) + "-" + ('0' + yesterday.getDate()).slice(-2);
 }
 $(document).ready(function(){
-  var luckyNum;
-  if (!localStorage.imageOfTheDay && localStorage.viewDate !== getYesterdayDate()){
+  var luckyNum = 0;
+  if (true || !localStorage.imageOfTheDayUrl && localStorage.imageOfTheDayDate !== getYesterdayDate()){
     $.ajax({
       url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+getYesterdayDate()+"&api_key="+API_KEY,
       success: function(result){
         listOfImages = result;
         luckyNum = Math.round(Math.random() * listOfImages.photos.length+1);
-        localStorage.imageOfTheDay = listOfImages.photos[luckyNum].img_src;
-        document.getElementById("imageOfDay").src=listOfImages.photos[luckyNum].img_src;
-        localStorage.viewDate = listOfImages.photos[luckyNum].earth_date;
+        localStorage.imageOfTheDayUrl = listOfImages.photos[luckyNum].img_src;
+        localStorage.imageOfTheDayDate = listOfImages.photos[luckyNum].earth_date;
+        localStorage.imageOfTheDayCameraName = listOfImages.photos[luckyNum].camera.full_name;
+        displayImageOfTheDay();
     }});
   }else{
     // no need to refetch, just use what's cached.
-    document.getElementById("imageOfDay").src= localStorage.imageOfTheDay;
+    displayImageOfTheDay();
   }
 });
+
+function displayImageOfTheDay(){
+  document.getElementById("imageOfDay").src = localStorage.imageOfTheDayUrl;
+  document.getElementById("imageOfDayName").innerHTML = localStorage.imageOfTheDayCameraName;
+  document.getElementById("imageOfDayDate").innerHTML = "Captured on " + localStorage.imageOfTheDayDate;
+}
+
+function shallowCloneObj(obj) {
+  var newObj = {};
+  for (var i in obj){
+    if(typeof(obj[i]) === 'object')
+      newObj[i] = shallowCloneObj(obj[i]);
+    else
+     newObj[i] = obj[i];
+   }
+  return newObj;
+}
